f9f34b1fcc978fe519fc685a30de9b7dfe587dbb
Charles Cox <charles.h.cox@gmail.com>
2021-08-27 13:49:48 -0500

    Update fans20exec.sh

--- fans20exec.sh
+++ fans20exec.sh
@@ -3,7 +3,7 @@
 # This puts the script to power the fans at 20%  in the proper directory.
 # The systemd unit will call it from here.
 
-cat > /usr/bin/ipmitool.sh << "EOF"
+sudo cat > /usr/bin/ipmitool.sh << "EOF"
 #!/bin/bash
 
 # takes manual control of the fans
@@ -14,5 +14,5 @@ ipmitool raw 0x30 0x30 0x02 0xff 0x14
 EOF
 
 # Give the script the proper permissions
-chown root:root /usr/bin/ipmitool.sh
-chmod +x /usr/bin/ipmitool.sh
+sudo chown root:root /usr/bin/ipmitool.sh
+sudo chmod +x /usr/bin/ipmitool.sh
