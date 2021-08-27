282a14ad67a9530163b8ab38385a5af82273e9d4
Charles Cox <charles.h.cox@gmail.com>
2021-08-27 00:17:03 -0500

    Update fans20exec.sh

--- fans20exec.sh
+++ fans20exec.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+cat > /usr/bin/ipmitool.sh << "EOF"
+#!/bin/bash
+
+# takes manual control of the fans
+ipmitool raw 0x30 0x30 0x01 0x00
+
+# sets the fans to 20%
+ipmitool raw 0x30 0x30 0x02 0xff 0x14
+EOF
+
+# Give the script the proper permissions
+chown root:root /usr/bin/ipmitool.sh
+chmod +x /usr/bin/ipmitool.sh
 
